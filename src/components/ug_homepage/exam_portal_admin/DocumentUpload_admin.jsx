e54b734c2675b0fff70cb0c5f6e36403ff2d9326
AHarshitha04 <harshitha.a@egradtutor.in>
2023-12-16 15:46:28 +0530

    kjoj

--- src/components/ug_homepage/exam_portal_admin/DocumentUpload_admin.jsx
+++ src/components/ug_homepage/exam_portal_admin/DocumentUpload_admin.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
-
-
 import "./styles/DocumentUpload_admin.css";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const DocumentUpload_admin = () => {
   const [tests, setTests] = useState([]);
